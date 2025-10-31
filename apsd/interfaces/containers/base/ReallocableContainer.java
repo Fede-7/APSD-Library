e796919b33127f7302a340a555ba67f6d6913f68
Fede-7 <federicodellini@gmail.com>
2025-10-31 19:22:18 +0100

    Reallocable Container interface done

--- apsd/interfaces/containers/base/ReallocableContainer.java
+++ apsd/interfaces/containers/base/ReallocableContainer.java
@@ -1,30 +1,40 @@
 package apsd.interfaces.containers.base;
 
-// import apsd.classes.utilities.Natural;
-// import apsd.interfaces.traits.Reallocable;
+import apsd.classes.utilities.Natural;
+import apsd.interfaces.traits.Reallocable;
 
 /** Interface: ClearableContainer che è anche Reallocable. */
-public interface ReallocableContainer { // Must extend ClearableContainer, Reallocable
+public interface ReallocableContainer extends ClearableContainer, Reallocable {
 
   double GROW_FACTOR = 2.0; // Must be strictly greater than 1.
   double SHRINK_FACTOR = 2.0; // Must be strictly greater than 1.
 
-  // Capacity
+  Natural capacity();
 
-  // Grow
+  default void grow() {
+    Realloc(Natural.valueOf((long) (capacity().toLong() * GROW_FACTOR)));
+  }
 
-  // Shrink
+  default void grow(double factor) {
+    Realloc(Natural.valueOf((long) (capacity().toLong() * factor)));
+  }
+
+  default void shrink() {
+    Realloc(Natural.valueOf((long) (capacity().toLong() / SHRINK_FACTOR)));
+  }
 
   /* ************************************************************************ */
-  /* Override specific member functions from Container                        */
+  /* Override specific member functions from Container */
   /* ************************************************************************ */
 
-  // ...
-
+  @Override
+  default Natural size() { return capacity(); }
+  
   /* ************************************************************************ */
-  /* Override specific member functions from ClearableContainer               */
+  /* Override specific member functions from ClearableContainer */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  default void clear() { Realloc(Natural.ZERO);  }
 
 }
