8fda8cda4e690e158222669f7710af3d1ade234c
Fede-7 <federicodellini@gmail.com>
2025-10-31 18:47:13 +0100

    Container interface done

--- apsd/interfaces/containers/base/Container.java
+++ apsd/interfaces/containers/base/Container.java
@@ -1,12 +1,14 @@
 package apsd.interfaces.containers.base;
 
-// import apsd.classes.utilities.Natural;
+import apsd.classes.utilities.Natural;
 
 /** Interface: Container, la base di tutti i contenitori. */
 public interface Container {
 
   // Size
+  Natural size();
 
   // IsEmpty
+  boolean isEmpty();
 
 }
