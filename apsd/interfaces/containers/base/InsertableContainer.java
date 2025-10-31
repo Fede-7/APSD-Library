fe438d4d2b2fc2e180cb2c96fee6881d771ad831
Fede-7 <federicodellini@gmail.com>
2025-10-31 20:09:06 +0100

    insertable Container interface done

--- apsd/interfaces/containers/base/InsertableContainer.java
+++ apsd/interfaces/containers/base/InsertableContainer.java
@@ -1,12 +1,20 @@
 package apsd.interfaces.containers.base;
 
 /** Interface: Container con supporto all'inserimento di un dato. */
-public interface InsertableContainer<Data> { // Must extend Container
+public interface InsertableContainer<Data> extends Container{
 
-  // Insert
+  Boolean Insert(Data data);
 
-  // InsertAll
+  Boolean InsertAll(TraversableContainer<Data> con){
+    final Box<boolean> allInserted = new Box<>(true);
+    if(con != null) con.TraversFoward -> {dat -> allInserted.Set(allInserted.Get() && Insert(dat)); };
+    return allInserted.Get();
+  }
 
-  // InsertSome
+  Boolean InsertSome(TraversableContainer<Data> con){
+    final Box<boolean> someInserted = new Box<>(false);
+    if(con != null) con.TraversFoward -> {dat -> someInserted.Set(someInserted.Get() || Insert(dat)); };
+    return someInserted.Get();
+  }
 
 }
