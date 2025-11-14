c20491d934452bbad23ab8d55e2e89b757ecc0af
Fede-7 <federicodellini@gmail.com>
2025-11-14 21:37:37 +0100

    mutable C/Iter  done

--- apsd/interfaces/containers/base/MutableIterableContainer.java
+++ apsd/interfaces/containers/base/MutableIterableContainer.java
@@ -1,11 +1,12 @@
 package apsd.interfaces.containers.base;
 
-// import apsd.interfaces.containers.iterators.MutableBackwardIterator;
-// import apsd.interfaces.containers.iterators.MutableForwardIterator;
+import apsd.interfaces.containers.iterators.MutableBackwardIterator;
+import apsd.interfaces.containers.iterators.MutableForwardIterator;
 
 /** Interface: IterableContainer con supporto all'iterazione mutabile. */
-public interface MutableIterableContainer<Data> { // Must extend IterableContainer
+public interface MutableIterableContainer<Data> extends IterableContainer<Data> { 
 
-  // ...
+  MutableForwardIterator<Data> FMutIterator();
+  MutableBackwardIterator<Data> BMutIterator();
 
 }
