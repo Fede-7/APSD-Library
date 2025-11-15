deda01d81ceb956420b707ff22f4b43e8c482446
Fede-7 <federicodellini@gmail.com>
2025-11-16 00:55:41 +0100

    SortSequence save

--- apsd/interfaces/containers/sequences/SortedSequence.java
+++ apsd/interfaces/containers/sequences/SortedSequence.java
@@ -1,16 +1,17 @@
 package apsd.interfaces.containers.sequences;
 
-// import apsd.classes.utilities.Natural;
-// import apsd.interfaces.containers.base.SortedIterableContainer;
+import apsd.classes.utilities.Natural;
+import apsd.interfaces.containers.base.SortedIterableContainer;
 
 /** Interface: Sequence & SortedIterableContainer. */
-public interface SortedSequence<Data> { // Must extend Sequence and SortedIterableContainer
+public interface SortedSequence<Data extends Comparable<Data>> extends Sequence<Data>, SortedIterableContainer<Data> {
 
   /* ************************************************************************ */
   /* Override specific member functions from MembershipContainer              */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  default boolean Exists(Data dat) { return this.TraverseForward( elem -> elem.equals(dat)); }
 
   /* ************************************************************************ */
   /* Override specific member functions from Sequence                         */
