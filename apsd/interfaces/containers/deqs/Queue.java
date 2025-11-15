eef8a8d4110ddf252d1f039d347a076414472aef
Fede-7 <federicodellini@gmail.com>
2025-11-16 00:19:05 +0100

    Queue done

--- apsd/interfaces/containers/deqs/Queue.java
+++ apsd/interfaces/containers/deqs/Queue.java
@@ -1,26 +1,41 @@
 package apsd.interfaces.containers.deqs;
 
-// import apsd.interfaces.containers.base.ClearableContainer;
-// import apsd.interfaces.containers.base.InsertableContainer;
+import apsd.interfaces.containers.base.ClearableContainer;
+import apsd.interfaces.containers.base.InsertableContainer;
 
-public interface Queue<Data> { // Must extend ClearableContainer and InsertableContainer
+public interface Queue<Data> extends ClearableContainer, InsertableContainer<Data> {
 
-  // Head
-  // Dequeue
-  // HeadNDequeue
+  Data head();
+  void dequeue();
+  void enqueue(Data element);
 
-  // Enqueue
+  default Data headNDequeue(){
+    final Data element = this.head();
+    this.dequeue();
+    return element;
+  }
+
+  
 
   /* ************************************************************************ */
   /* Override specific member functions from ClearableContainer               */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  default void Clear() {
+    while (this.Size().ToLong() > 0) {
+      this.dequeue();
+    }
+  }
 
   /* ************************************************************************ */
   /* Override specific member functions from InsertableContainer              */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  default boolean Insert(Data element) {
+    this.enqueue(element);
+    return true;
+  }
 
 }
