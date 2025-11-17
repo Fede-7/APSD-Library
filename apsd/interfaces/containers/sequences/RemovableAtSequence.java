1c3b9bf0549cd0603a2519e91ad897a3707957ef
Fede-7 <federicodellini@gmail.com>
2025-11-17 18:19:06 +0100

    RemvSeq save

--- apsd/interfaces/containers/sequences/RemovableAtSequence.java
+++ apsd/interfaces/containers/sequences/RemovableAtSequence.java
@@ -5,24 +5,35 @@ import apsd.classes.utilities.Natural;
 /** Interface: Sequence con supporto alla rimozione di un dato tramite posizione. */
 public interface RemovableAtSequence<Data> extends Sequence<Data> {
 
-  default void RemoveAt(Natural pos){
-    final long idx = ExcIfOutOfBound(pos);
-    this.TraverseForward(dat -> {
-      if(idx == 0){
-        dat = null;
-        return true;
-      }
-      idx = ;
-      return false;
-    });
+  // default void RemoveAt(Natural pos){
+  //   long[] idx = new long[1];
+  //   idx[0] = ExcIfOutOfBound(pos);
+  //   this.TraverseForward(dat -> {
+  //     if(idx[0] == 0){
+  //       dat = null;
+  //       return true;
+  //     }
+  //     idx[0]--;
+  //     return false;
+  //   });
+  // }
+
+  // Data AtNRemove(Natural pos);
+  
+  void RemoveAt(Natural pos);
+
+  default Data AtNRemove(Natural pos) {
+    if (!IsEmpty() && IsInBound(pos)) {
+      GetAt(pos);
+      RemoveAt(pos);
+    }
+    return null;
   }
 
-  // AtNRemove
+  default void RemoveFirst(){if(!IsEmpty()) RemoveAt(Natural.ZERO);};
+  default Data FirstNRemove(){return AtNRemove(Natural.ZERO);};
 
-  // RemoveFirst
-  // FirstNRemove
-
-  // RemoveLast
-  // LastNRemove
+  default void RemoveLast(){if(!IsEmpty()) RemoveAt(Size().Decrement());};
+  default Data LastNRemove(){return AtNRemove(Size().Decrement());};
 
 }
