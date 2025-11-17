f5230b4f72bf95a8cd9e6fe80d2781f4a8dbbf90
Fede-7 <federicodellini@gmail.com>
2025-11-17 14:00:07 +0100

    RemvSeq save

--- apsd/interfaces/containers/sequences/RemovableAtSequence.java
+++ apsd/interfaces/containers/sequences/RemovableAtSequence.java
@@ -1,11 +1,22 @@
 package apsd.interfaces.containers.sequences;
 
-// import apsd.classes.utilities.Natural;
+import apsd.classes.utilities.Natural;
 
 /** Interface: Sequence con supporto alla rimozione di un dato tramite posizione. */
-public interface RemovableAtSequence<Data> { // Must extend Sequence
+public interface RemovableAtSequence<Data> extends Sequence<Data> {
 
-  // RemoveAt
+  default void RemoveAt(Natural pos){
+    final long idx = ExcIfOutOfBound(pos);
+    this.TraverseForward(dat -> {
+      if(idx == 0){
+        dat = null;
+        return true;
+      }
+      idx = ;
+      return false;
+    });
+  }
+
   // AtNRemove
 
   // RemoveFirst
