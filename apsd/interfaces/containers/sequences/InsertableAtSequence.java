b2d614c282f299ce2e3cdc00818d2a80f9cf7416
Fede-7 <federicodellini@gmail.com>
2025-11-17 18:31:21 +0100

    InseAtSeq save

--- apsd/interfaces/containers/sequences/InsertableAtSequence.java
+++ apsd/interfaces/containers/sequences/InsertableAtSequence.java
@@ -1,14 +1,14 @@
 package apsd.interfaces.containers.sequences;
 
-// import apsd.classes.utilities.Natural;
+import apsd.classes.utilities.Natural;
 
 /** Interface: Sequence con supporto all'inserimento di un dato tramite posizione. */
-public interface InsertableAtSequence<Data> { // Must extend Sequence
+public interface InsertableAtSequence<Data> extends Sequence<Data>{ 
 
-  // InsertAt
+  void InsertAt(Data elem, Natural pos);
 
-  // InsertFirst
+  default void InsertFirst(Data elem){InsertAt(elem, Natural.ZERO);}
 
-  // InsertLast
+  default void InsertLast(Data dat){InsertAt(dat, Size().Decrement());}
 
 }
