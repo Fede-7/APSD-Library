df47aa46ab9fe3de1d44906ac9e6f7ed294f41e7
Fede-7 <federicodellini@gmail.com>
2025-11-19 20:42:19 +0100

    OrderSet done

--- apsd/interfaces/containers/collections/OrderedSet.java
+++ apsd/interfaces/containers/collections/OrderedSet.java
@@ -1,29 +1,63 @@
 package apsd.interfaces.containers.collections;
 
-public interface OrderedSet<Data> { // Must extend Set
+public interface OrderedSet<Data extends Comparable<Data>> extends Set<Data> {
 
-  // Min
+  default Data Min() {return (this.IsEmpty()) ? null : FIterator().GetCurrent();}
 
-  // RemoveMin
+  default void RemoveMin(){if(!this.IsEmpty()) Remove(Min());}
 
-  // MinNRemove
+  default Data MinNRemove(){
+    if(this.IsEmpty())return null;
 
-  // Max
+    Data min = this.Min();
+    RemoveMin();
+    return min;
+  }
 
-  // RemoveMax
+  default Data Max(){return (this.IsEmpty()) ? null : BIterator().GetCurrent();}
 
-  // MaxNRemove
+  default void RemoveMax(){if(!this.IsEmpty()) Remove(Max());}
 
-  // Predecessor
+  default Data MaxNRemove(){
+    if(this.IsEmpty()) return null;
 
-  // RemovePredecessor
+    Data max = Max();
+    RemoveMax();
+    return max;
+  }
 
-  // PredecessorNRemove
+  default Data Predecessor(Data dat){
+    if(this.IsEmpty())return null;
 
-  // Successor
+    return this.FoldForward(
+      (num, acc) -> (num.compareTo(dat) < 0) ? num : acc, null);
+  }
 
-  // RemoveSuccessor
+  default void RemovePredecessor(Data dat){if(!IsEmpty())this.Remove(Predecessor(dat));}
 
-  // SuccessorNRemove
+  default Data PredecessorNRemove(Data dat){
+    if(this.IsEmpty())return null;
+
+    Data pred = Predecessor(dat);
+    this.RemovePredecessor(dat);
+    return pred;
+  }
+
+  default Data Successor(Data dat){
+    if(this.IsEmpty())return null;
+
+    return this.FoldBackward(
+      (num, acc) -> (num.compareTo(dat) > 0) ? num : acc, null);
+  }
+
+  default void RemoveSuccessor(Data dat){if(!IsEmpty())this.Remove(Successor(dat));}
+
+  default Data SuccessorNRemove(Data dat){
+    if(this.IsEmpty())return null;
+
+    Data succ = Successor(dat);
+    this.RemoveSuccessor(dat);
+    return succ;
+  }
 
 }
