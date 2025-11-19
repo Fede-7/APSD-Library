fdd3d1aa2d1961df785cc749577b6722ee114094
Fede-7 <federicodellini@gmail.com>
2025-11-20 00:49:50 +0100

    Chain save

--- apsd/interfaces/containers/collections/Chain.java
+++ apsd/interfaces/containers/collections/Chain.java
@@ -1,20 +1,46 @@
 package apsd.interfaces.containers.collections;
 
-// import apsd.classes.utilities.Natural;
-// import apsd.interfaces.containers.sequences.RemovableAtSequence;
+import apsd.classes.utilities.Natural;
+import apsd.interfaces.containers.sequences.RemovableAtSequence;
 
-public interface Chain<Data> { // Must extend RemovableAtSequence
+public interface Chain<Data> extends RemovableAtSequence<Data>, Set<Data>{
+  default boolean InsertIfAbsent(Data dat){
+    return (!this.IsEmpty() && !this.Exists(dat)) ? this.Insert(dat) : false;
+  }
 
-  // InsertIfAbsent
+  default void RemoveOccurrences(Data dat){
+    if(this.IsEmpty()) return;
+    this.Filter(elm -> elm.equals(dat));
+  }
 
-  // RemoveOccurrences
+  @SuppressWarnings("unchecked")
+  default Chain<Data> SubChain(Natural start, Natural finish) {
+    //TODO: controllore se questo metodo è corretto
+    Chain<Data> subChain;
+    try {
+      subChain = (Chain<Data>) this.getClass().getDeclaredConstructor().newInstance();
+    } catch (Exception e) {
+      throw new RuntimeException("Failed to create subchain instance", e);
+    }
 
-  // SubChain
+    long idx = 0;
+    long str = ExcIfOutOfBound(start);
+    long fns = ExcIfOutOfBound(finish);
+
+    for (; str <= fns; str++, idx++) {
+      subChain.Insert(this.GetAt(Natural.Of(idx)));
+    }
+
+    return subChain;
+  }
 
   /* ************************************************************************ */
   /* Override specific member functions from Sequence                         */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  default Natural Search(Data dat){
+
+  }
 
 }
