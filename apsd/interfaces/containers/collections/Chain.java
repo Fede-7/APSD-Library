a33b4f882e12bdf307f0fc123ec61e77a682e645
Fede-7 <federicodellini@gmail.com>
2025-11-20 12:47:51 +0100

    Chain done

--- apsd/interfaces/containers/collections/Chain.java
+++ apsd/interfaces/containers/collections/Chain.java
@@ -5,13 +5,11 @@ import apsd.interfaces.containers.sequences.RemovableAtSequence;
 
 public interface Chain<Data> extends RemovableAtSequence<Data>, Set<Data>{
   
-  default boolean InsertIfAbsent(Data dat){
-    return (!this.IsEmpty() && !this.Exists(dat)) ? this.Insert(dat) : false;
-  }
+  default boolean InsertIfAbsent(Data dat){ return (!this.Exists(dat)) ? this.Insert(dat) : false;}
 
   default void RemoveOccurrences(Data dat){
     if(this.IsEmpty()) return;
-    this.Filter(elm -> elm.equals(dat));
+    this.Filter(elm -> !elm.equals(dat));
   }
   
 //TODO: controllore se questo metodo è corretto
@@ -25,12 +23,11 @@ public interface Chain<Data> extends RemovableAtSequence<Data>, Set<Data>{
       throw new RuntimeException("Failed to create subchain instance", e);
     }
 
-    long idx = 0;
     long str = ExcIfOutOfBound(start);
     long fns = ExcIfOutOfBound(finish);
 
-    for (; str <= fns; str++, idx++) {
-      subChain.Insert(this.GetAt(Natural.Of(idx)));
+    for (; str <= fns; str++) {
+      subChain.Insert(this.GetAt(Natural.Of(str)));
     }
 
     return subChain;
@@ -42,7 +39,10 @@ public interface Chain<Data> extends RemovableAtSequence<Data>, Set<Data>{
 
   @Override
   default Natural Search(Data dat){
-    
+    if(this.IsEmpty() || dat == null)return null;
+    long idx = 0;
+    while(idx < Size().ToLong() && !this.GetAt(Natural.Of(idx)).equals(dat)){idx++;}
+    return (idx < Size().ToLong()) ? Natural.Of(idx) : null;
   }
 
 }
