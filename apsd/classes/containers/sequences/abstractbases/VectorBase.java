b6e5c56c11ac62641b7748ae94ec250d1124c0b2
Fede-7 <federicodellini@gmail.com>
2025-11-27 19:11:36 +0100

    VectorBase save

--- apsd/classes/containers/sequences/abstractbases/VectorBase.java
+++ apsd/classes/containers/sequences/abstractbases/VectorBase.java
@@ -1,57 +1,118 @@
 package apsd.classes.containers.sequences.abstractbases;
 
-// import apsd.classes.utilities.MutableNatural;
-// import apsd.classes.utilities.Natural;
-// import apsd.interfaces.containers.base.TraversableContainer;
-// import apsd.interfaces.containers.iterators.MutableBackwardIterator;
-// import apsd.interfaces.containers.iterators.MutableForwardIterator;
-// import apsd.interfaces.containers.sequences.MutableSequence;
-// import apsd.interfaces.containers.sequences.Vector;
+import apsd.classes.utilities.MutableNatural;
+import apsd.classes.utilities.Natural;
+import apsd.interfaces.containers.base.TraversableContainer;
+import apsd.interfaces.containers.iterators.BackwardIterator;
+import apsd.interfaces.containers.iterators.ForwardIterator;
+import apsd.interfaces.containers.iterators.MutableBackwardIterator;
+import apsd.interfaces.containers.iterators.MutableForwardIterator;
+import apsd.interfaces.containers.sequences.MutableSequence;
+import apsd.interfaces.containers.sequences.Vector;
 
 /** Object: Abstract vector base implementation. */
-abstract public class VectorBase<Data> { // Must implement Vector
+abstract public class VectorBase<Data> implements Vector<Data>{
 
-  // protected Data[] arr;
+  protected Data[] arr;
 
-  // VectorBase
+  //TODO: find Vector base info?
+  public VectorBase(Natural inisize){
+    if (inisize == null) throw new NullPointerException("Natural cannot be a null value");
+    ArrayAlloc(inisize);
+  }
 
-  // NewVector
+  protected void NewVector(Data[] arr){this.arr = arr;}
 
-  // @SuppressWarnings("unchecked")
-  // protected void ArrayAlloc(Natural newsize) {
-  //   long size = newsize.ToLong();
-  //   if (size >= Integer.MAX_VALUE) { throw new ArithmeticException("Overflow: size cannot exceed Integer.MAX_VALUE!"); }
-  //   arr = (Data[]) new Object[(int) size];
-  // }
+  @SuppressWarnings("unchecked")
+  protected void ArrayAlloc(Natural newsize) {
+    long size = newsize.ToLong();
+    if (size >= Integer.MAX_VALUE) { throw new ArithmeticException("Overflow: size cannot exceed Integer.MAX_VALUE!"); }
+    arr = (Data[]) new Object[(int) size];
+  }
 
   /* ************************************************************************ */
   /* Override specific member functions from ClearableContainer               */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  public void Clear() {ArrayAlloc(Natural.ZERO);}
 
   /* ************************************************************************ */
   /* Override specific member functions from ResizableContainer               */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  public Natural Capacity() {return Natural.Of(((long) arr.length));}
 
   /* ************************************************************************ */
   /* Override specific member functions from IterableContainer                */
   /* ************************************************************************ */
 
-  // ...
+  protected class VectorFIterator implements MutableForwardIterator<Data>{
+    protected long idxCur = 0L;
+
+    //TODO: IsValid vuole Size or Capacity?
+    @Override
+    public boolean IsValid() {return (0 <= idxCur && idxCur < Size().ToLong());}
+
+    @Override
+    public Data GetCurrent() { 
+      if (!IsValid()) throw new IllegalStateException(" Iterator is not valid!");
+      return arr[(int) idxCur];
+    }
+
+    @Override
+    public void SetCurrent(Data dat) {
+      if (!IsValid()) throw new IllegalStateException(" Iterator is not valid!");
+      arr[(int) idxCur] = dat;
+    }
+
+    @Override
+    public Data DataNNext() {
+      if (!IsValid()) throw new IllegalStateException(" Iterator is not valid!");
+      Data dat = GetCurrent();
+      idxCur++;
+      return dat;
+    }
+
+    @Override
+    public void Next() { DataNNext();}
+
+    @Override
+    public void Next(long steps) { for(; steps > 0; steps--){DataNNext();}}
+
+    @Override
+    public void Next(Natural steps) {Next(steps.ToLong());}
+
+    @Override
+    public void Reset() { idxCur = 0L;}
+
+  }
+
+  @Override
+  public MutableForwardIterator<Data> FIterator() { return new VectorFIterator();}
+
+  @Override
+  public MutableBackwardIterator<Data> BIterator() {
+      // TODO Auto-generated method stub
+      return null;
+  }
 
   /* ************************************************************************ */
   /* Override specific member functions from Sequence                         */
   /* ************************************************************************ */
 
-  // ...
+  @SuppressWarnings("unchecked")
+  @Override
+  public MutableSequence<Data> SubSequence(Natural start, Natural end) {
+      Data[] arrCopy;
+
+      return (MutableSequence) arrCopy;
+  }
 
   /* ************************************************************************ */
   /* Override specific member functions from MutableSequence                  */
   /* ************************************************************************ */
 
-  // ...
-
+  //TODO: ?? non lo so manco io
 }
