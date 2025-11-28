9b5f89fe08deb32c228c6f48d928c4ebff0af860
Friedrich <75255965+Fede-7@users.noreply.github.com>
2025-11-28 16:43:39 +0000

    Lin Vect  base save

--- apsd/classes/containers/sequences/abstractbases/LinearVectorBase.java
+++ apsd/classes/containers/sequences/abstractbases/LinearVectorBase.java
@@ -2,6 +2,7 @@ package apsd.classes.containers.sequences.abstractbases;
 
 import apsd.classes.utilities.Natural;
 import apsd.interfaces.containers.base.TraversableContainer;
+import apsd.interfaces.containers.iterators.MutableForwardIterator;
 
 /** Object: Abstract (static) linear vector base implementation. */
 abstract public class LinearVectorBase<Data> extends VectorBase<Data> { // Must extend VectorBase
@@ -13,26 +14,43 @@ abstract public class LinearVectorBase<Data> extends VectorBase<Data> { // Must 
   /* ************************************************************************ */
 
   @Override
-  public void Realloc(Natural newsize) { if(newsize == null) throw new NullPointerException("Natural cannot be a null value");
+  public void Realloc(Natural newsize) { 
+    if(newsize == null) throw new NullPointerException("Natural cannot be a null value");
     Data[] newarr;
     long size = newsize.ToLong();
     if (size >= Integer.MAX_VALUE) { throw new ArithmeticException("Overflow: size cannot exceed Integer.MAX_VALUE!"); }
     newarr = (Data[]) new Object[(int) size];
     long minsize = Math.min(arr.length, (int) size);
+
+    //TODO: Verificare il funzionamento di arraycopy - i don't trust you @Copilot
     System.arraycopy(arr, 0, newarr, 0, (int) minsize);
-    arr = newarr;
+    arr = newarr; 
   }
 
   /* ************************************************************************ */
   /* Override specific member functions from Sequence                         */
   /* ************************************************************************ */
+ 
+  @Override
+  public Data GetAt(Natural pos) {
+     if(pos == null) throw new NullPointerException("Natural cannot be a null value");
+     MutableForwardIterator<Data> Iter = FIterator();
+     Iter.Next(ExcIfOutOfBound(pos));
+     return Iter.GetCurrent();
+  }
 
-  // ...
+  @Override
+  public void SetAt(Data elem, Natural pos) {
+    if(pos == null) throw new NullPointerException("Natural cannot be a null value");
+    MutableForwardIterator<Data> Iter = FIterator();
+    Iter.Next(ExcIfOutOfBound(pos));
+    Iter.SetCurrent(elem);
+  }
 
   /* ************************************************************************ */
   /* Override specific member functions from MutableSequence                  */
   /* ************************************************************************ */
 
-  // ...
+  //TODO: Check this method wtf is going on here
 
 }
