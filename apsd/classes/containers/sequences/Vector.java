11871051c6d99a8a1ce2c988ae26eb6e31777a7e
Fede-7 <federicodellini@gmail.com>
2025-12-02 19:20:47 +0100

    Refactor Vector class to extend LinearVectorBase; restore constructor implementations and method overrides

--- apsd/classes/containers/sequences/Vector.java
+++ apsd/classes/containers/sequences/Vector.java
@@ -1,20 +1,49 @@
 package apsd.classes.containers.sequences;
 
-// import apsd.classes.containers.sequences.abstractbases.LinearVectorBase;
-// import apsd.classes.utilities.Natural;
-// import apsd.interfaces.containers.base.TraversableContainer;
+import apsd.classes.containers.sequences.abstractbases.LinearVectorBase;
+import apsd.classes.containers.sequences.abstractbases.VectorBase;
+import apsd.classes.utilities.Natural;
+import apsd.interfaces.containers.base.TraversableContainer;
+import apsd.interfaces.containers.iterators.MutableBackwardIterator;
+import apsd.interfaces.containers.iterators.MutableForwardIterator;
 
 /** Object: Concrete (static linear) vector implementation. */
-public class Vector<Data> { // Must extend LinearVectorBase
+public class Vector<Data> extends LinearVectorBase<Data>{
 
-  // public Vector()
+  public Vector() {
+    super();
+    ArrayAlloc(Natural.ONE);
+  }
 
-  // public Vector(Natural inisize)
+  public Vector(Natural inisize) {
+    super();
+    ArrayAlloc(inisize);
+  }
 
-  // public Vector(TraversableContainer<Data> con)
+  public Vector(TraversableContainer<Data> con) {
+    super();
+    ArrayAlloc(new Natural(con.Size()));
+    MutableForwardIterator<Data> it = FIterator();
+    int i = 0;
+    while (!it.IsValid()) {
+      arr[i++] = it.DataNNext();
+    }
+  }
 
-  // protected Vector(Data[] arr)
+  protected Vector(Data[] arr) {
+    super();
+    arr = arr;
+  }
+  
+  @Override
+  protected VectorBase<Data> NewVector(Data[] arr) {
+    return new Vector<Data>(arr);
+  }
 
-  // NewVector
+  @Override
+  public MutableForwardIterator<Data> FMutIterator() { return FIterator(); }
+
+  @Override
+  public MutableBackwardIterator<Data> BMutIterator() { return BIterator(); }
 
 }
