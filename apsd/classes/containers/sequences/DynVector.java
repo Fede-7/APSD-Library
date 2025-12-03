ad5f335e1c893f9d1a79a344767a68a0b275fd0a
Fede-7 <federicodellini@gmail.com>
2025-12-03 20:26:10 +0100

    Refactor DynVector class; restore inheritance from DynLinearVectorBase and reintroduce constructors

--- apsd/classes/containers/sequences/DynVector.java
+++ apsd/classes/containers/sequences/DynVector.java
@@ -1,20 +1,29 @@
 package apsd.classes.containers.sequences;
 
-// import apsd.classes.containers.sequences.abstractbases.DynLinearVectorBase;
-// import apsd.classes.utilities.Natural;
-// import apsd.interfaces.containers.base.TraversableContainer;
+import apsd.classes.containers.sequences.abstractbases.DynLinearVectorBase;
+import apsd.classes.utilities.Natural;
+import apsd.interfaces.containers.base.TraversableContainer;
+import apsd.interfaces.containers.iterators.MutableBackwardIterator;
+import apsd.interfaces.containers.iterators.MutableForwardIterator;
 
 /** Object: Concrete dynamic (linear) vector implementation. */
-public class DynVector<Data> { // Must extend DynLinearVectorBase
+public class DynVector<Data> extends DynLinearVectorBase<Data> {
 
-  // public DynVector()
+  public DynVector(){ super();}
 
-  // public DynVector(Natural inisize)
+  public DynVector(Natural inisize){ super(inisize);}
 
-  // public DynVector(TraversableContainer<Data> con)
+  public DynVector(TraversableContainer<Data> con){ super(con);}
 
-  // protected DynVector(Data[] arr)
+  protected DynVector(Data[] arr){ super(arr);}
 
-  // NewVector
+  public DynVector<Data> NewVector(Data[] arr){ return new DynVector<>();}
+
+  @Override
+  public MutableForwardIterator<Data> FMutIterator() { return FIterator(); }
+
+  @Override
+  public MutableBackwardIterator<Data> BMutIterator() { return BIterator(); }
+
 
 }
