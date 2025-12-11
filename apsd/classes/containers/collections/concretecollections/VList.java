ff53577ec204efbebda7bcf4689b683b265ff492
Fede-7 <federicodellini@gmail.com>
2025-12-11 18:14:21 +0100

    Refactor VList class to improve structure and readability; restore necessary imports, enhance method implementations, and clarify comments.

--- apsd/classes/containers/collections/concretecollections/VList.java
+++ apsd/classes/containers/collections/concretecollections/VList.java
@@ -2,42 +2,60 @@ package apsd.classes.containers.collections.concretecollections;
 
 import apsd.interfaces.containers.sequences.DynVector;
 
-// import apsd.classes.containers.collections.concretecollections.bases.VChainBase;
-// import apsd.classes.utilities.Natural;
-// import apsd.interfaces.containers.base.TraversableContainer;
-// import apsd.interfaces.containers.collections.List;
-// import apsd.interfaces.containers.iterators.MutableBackwardIterator;
-// import apsd.interfaces.containers.iterators.MutableForwardIterator;
-// import apsd.interfaces.containers.sequences.DynVector;
-// import apsd.interfaces.containers.sequences.MutableSequence;
+import apsd.classes.containers.collections.concretecollections.bases.VChainBase;
+import apsd.classes.utilities.Natural;
+import apsd.interfaces.containers.base.TraversableContainer;
+import apsd.interfaces.containers.collections.List;
+import apsd.interfaces.containers.iterators.MutableBackwardIterator;
+import apsd.interfaces.containers.iterators.MutableForwardIterator;
+import apsd.interfaces.containers.sequences.DynVector;
+import apsd.interfaces.containers.sequences.MutableSequence;
 
 /** Object: Concrete list implementation on (dynamic circular) vector. */
-public class VList<Data> { // Must extend VChainBase and implement List
+public class VList<Data>  extends VChainBase<Data> implements List<Data>{
 
-  // public VList()
+  public VList(){super();}
 
-  // public VList(TraversableContainer<Data> con)
+  public VList(TraversableContainer<Data> con){super(con);}
 
-  // protected VList(DynVector<Data> vec)
+  protected VList(DynVector<Data> vec){super(vec);}
 
-  // NewChain
+  public VChainBase<Data> NewChain(DynVector<Data> vec){return new VList<>(vec);}
 
   /* ************************************************************************ */
   /* Override specific member functions from MutableIterableContainer         */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  public MutableForwardIterator<Data> FIterator() {
+    return vec.FMutIterator();
+  }
+  
+  @Override
+  public MutableBackwardIterator<Data> BIterator() {
+    return vec.BMutIterator();
+  }
 
   /* ************************************************************************ */
   /* Override specific member functions from MutableSequence                  */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  public void SetAt(Data elem, Natural pos) { vec.SetAt(elem, pos);}
+
+
+  //TODO: here he want mutablesequenz - aro cazz o miett?
+  @Override
+  public VChainBase<Data> SubSequence(Natural start, Natural end) {
+    if(!IsInBound(start) || !IsInBound(end)) return null;
+    return NewChain((DynVector<Data>) vec.SubSequence(start, end));
+  }
 
   /* ************************************************************************ */
   /* Override specific member functions from InsertableAtSequence             */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  public void InsertAt(Data elem, Natural pos) { vec.InsertAt(elem, pos);}
 
 }
