0dd9e88d9284888ee043665dd47434616f5d95f1
Fede-7 <federicodellini@gmail.com>
2025-12-11 11:50:15 +0100

    Refactor VSortedChain class to enhance functionality and readability; implement proper generics, improve Insert and RemoveOccurrences methods, and add missing methods for better encapsulation.

--- apsd/classes/containers/collections/concretecollections/VSortedChain.java
+++ apsd/classes/containers/collections/concretecollections/VSortedChain.java
@@ -1,34 +1,75 @@
 package apsd.classes.containers.collections.concretecollections;
 
-// import apsd.classes.containers.collections.concretecollections.bases.VChainBase;
-// import apsd.classes.utilities.Natural;
-// import apsd.interfaces.containers.base.TraversableContainer;
-// import apsd.interfaces.containers.collections.SortedChain;
-// import apsd.interfaces.containers.sequences.DynVector;
+import apsd.classes.containers.collections.concretecollections.bases.VChainBase;
+import apsd.classes.containers.sequences.DynCircularVector;
+import apsd.classes.utilities.Natural;
+import apsd.interfaces.containers.base.TraversableContainer;
+import apsd.interfaces.containers.collections.SortedChain;
+import apsd.interfaces.containers.sequences.DynVector;
 
 /** Object: Concrete set implementation via (dynamic circular) vector. */
-public class VSortedChain<Data> { // Must extend VChainBase and implements SortedChain
+public class VSortedChain<Data extends Comparable<Data>> extends VChainBase<Data> implements SortedChain<Data> {
 
-  // public VSortedChain()
+  public VSortedChain(){super();}
 
-  // public VSortedChain(VSortedChain<Data> chn)
+  public VSortedChain(VSortedChain<Data> chn){super(chn.vec);}
 
-  // public VSortedChain(TraversableContainer<Data> con)
+  public VSortedChain(TraversableContainer<Data> con){super(con);}
 
-  // protected VSortedChain(DynVector<Data> vec)
+  protected VSortedChain(DynVector<Data> vec){super(vec);}
 
-  // NewChain
+  public VSortedChain<Data> NewChain(VSortedChain<Data> chn){ return new VSortedChain<Data>(chn);}
 
   /* ************************************************************************ */
   /* Override specific member functions from InsertableContainer              */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  public boolean Insert(Data data) {
+    if (data == null || this.vec == null) return false;
+    Natural idx = SearchSuccessor(data);
+    idx = ( idx == null) ? Size().Decrement() : idx;
+    ShiftRight(idx);
+    InsertAt(data, idx);
+    return true;  
+  }
 
   /* ************************************************************************ */
   /* Override specific member functions from Chain                            */
   /* ************************************************************************ */
+/*
+*1. if dat = null:   Search::dat return null, than insert check and return false
+*2. if dat == Data:  Search::dat return null if its isnt presente, the idx if it's present.
+*/
+  @Override
+  public boolean InsertIfAbsent(Data dat) { return (Search(dat) == null) ? Insert(dat) : false;}
 
-  // ...
+  @Override
+  public void RemoveOccurrences(Data dat) {
+    if(dat == null || this.vec == null) return;
+    Filter(elm -> dat.equals(elm));
+ }
+
+  //TODO: metodi aggiunti per l'errore della classe
+  @Override
+  public Natural Capacity() { return vec.Capacity(); }
+
+  @Override
+  public void Expand(Natural size) { vec.Expand(size); }
+
+  @Override
+  public void Reduce(Natural size) { vec.Reduce(size); }
+
+  @Override
+  public void Realloc(Natural size) { vec.Realloc(size); }
+
+  @Override
+  protected VSortedChain<Data> NewChain(DynVector<Data> v) { return new VSortedChain<>(v); }
+
+  @Override
+  public MutableForwardIterator<Data> FMutIterator() { return vec.FMutIterator(); }
+
+  @Override
+  public MutableBackwardIterator<Data> BMutIterator() { return vec.BMutIterator(); }
 
 }
