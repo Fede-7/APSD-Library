3727a2d95a8aa30b95cf6fe6a27aa3c4c3caf306
Fede-7 <federicodellini@gmail.com>
2025-12-12 19:14:02 +0100

    Remove obsolete test interfaces and related JAR dependencies from the test framework, including CollectionTest, ListTest, OrderedSetTest, SetTest, SortedChainTest, QueueTest, StackTest, and various sequence tests. This cleanup enhances maintainability and reduces unnecessary complexity in the codebase.

--- zapsdtest/simpletest/apsd/classes/containers/collections/abstractcollections/generic/WSetTest.java
+++
@@ -1,7 +0,0 @@
-package zapsdtest.simpletest.apsd.classes.containers.collections.abstractcollections.generic;
-
-import apsd.interfaces.containers.collections.Set;
-import zapsdtest.testframework.containers.ContainerBaseTest;
-import zapsdtest.testframework.containers.collections.SetTest;
-
-abstract public class WSetTest<Data> extends ContainerBaseTest<Set<Data>> implements SetTest<Data, Set<Data>> {}
